0bd26216e1d3d686ada343e2c5221066e04c186a
gideonlexy <gideonlexy@gmail.com>
2020-01-27 08:13:15 +0300

    feature:Added header logo and login container to the home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
+import logo2 from './assets/logo2.png'
+import user from './assets/user.png'
 
 function App() {
   return (
     <div className="App">
+      <div className='header'>
+        <div className='logo'>
+          <img src={logo2} alt='Logo' />
+        </div>
+      </div>
+      <div className='wrapper'>
+        <div className='main'>
+          <div className='aside'>
+            <img src={user} alt='user' />
+          </div>
+          <div className='content'>
+            <div className='login-form'>
+              Login Form
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   );
