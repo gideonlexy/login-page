cd05be9a72b470470b29e55f16aa241d71d10b20
gideonlexy <gideonlexy@gmail.com>
2020-01-28 10:39:59 +0300

    feature: styled login form with css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import './App.css';
 import logo2 from './assets/logo2.png'
-import user from './assets/user.png'
+import background from './assets/back.jpg'
+import users from './assets/user.svg'
+import lock from './assets/lock.svg'
+import head from './assets/head.png'
 
 function App() {
   return (
-    <div className="App">
-      <div className='header'>
-        <div className='logo'>
-          <img src={logo2} alt='Logo' />
+    <div className="wrapper" style={{ backgroundImage: background }}>
+      <header>
+        <img src={head} alt='logo' style={{ height: '50px' }} />
+      </header>
+      <div className='login'>
+        <div className='left' >
+          <img className='logo' src={logo2} alt='logo' />
         </div>
-      </div>
-      <div className='wrapper'>
-        <div className='main'>
-          <div className='aside'>
-            <img src={user} alt='user' />
-          </div>
-          <div className='content'>
-            <div className='login-form'>
-              Login Form
+        <div className='right'>
+          <form>
+            <p><b><span className='dash'>Login</span> as Admin</b></p>
+            <div className='form-group'>
+              <input type='email' placeholder='jondoe@gmail.com' />
+              <img src={users} alt='User' />
             </div>
-          </div>
+
+            <div className='form-group'>
+              <input type='password' placeholder='xxxxxx' />
+              <img src={lock} alt='Lock' />
+
+            </div>
+            <button>LOGIN</button>
+            <div className='center'>
+              <p className='gray'>Forgot Password</p>
+              <p><a href='google.com'>Get help sign in</a></p>
+            </div>
+          </form>
+          <p className='center' style={{ marginTop: '80px' }}><small className='gray'>Terms of use and Privacy Policy</small></p>
         </div>
       </div>
-
     </div>
+
+
   );
 }
 
